d2ca8cdf3faf80ce0dbba7a90c7df5363b91e745
MagicHacker <zxmagichacker@gmail.com>
2020-01-17 17:11:15 +0800

    feat: 引入form组件

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import { Button } from "element-ui";
-Vue.use(Button);
+import { Button, Form, FormItem, Input } from "element-ui";
+[Button, Form, FormItem, Input].forEach(item => {
+  Vue.use(item);
+});
 Vue.config.productionTip = false;
 
 new Vue({
