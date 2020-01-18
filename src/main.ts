f97ddf715056292948a99111325dbaf62b5c5105
Johnson <zxmagichacker@gmail.com>
2020-01-18 10:30:47 +0800

    library: 引入message组件

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import { Button, Form, FormItem, Input } from "element-ui";
+import { Button, Form, FormItem, Input, Message } from "element-ui";
 [Button, Form, FormItem, Input].forEach(item => {
   Vue.use(item);
 });
+Vue.prototype.$message = Message;
 Vue.config.productionTip = false;
 
 new Vue({
