5ecf1ddccea25ae338607c814f6d66c93e523622
Johnson <zxmagichacker@gmail.com>
2020-01-20 21:12:16 +0800

    feat: 将header组件拆分出来

---
+++ src/components/lego-header/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <el-header class="main-page-header">
+    <div class="header-logo">
+      <icon-svg :icon-class="iconClass"></icon-svg>
+      Lego H5
+    </div>
+    <div class="header-person">
+      <el-popover placement="bottom" width="100px" trigger="hover">
+        <ul class="header-person-menu">
+          <li>个人中心</li>
+          <li>退出登录</li>
+        </ul>
+        <div slot="reference">
+          <img src="../../assets/images/joker.jpeg" alt="" />
+        </div>
+      </el-popover>
+    </div>
+  </el-header>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component
+export default class LegoHeader extends Vue {
+  iconClass: string = "lego";
+}
+</script>
+<style lang="less" scoped>
+.main-page-header {
+  height: 50px !important;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid #ccc;
+  .header-logo {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    cursor: pointer;
+  }
+  .header-person {
+    width: 44px;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    img {
+      width: 40px;
+      height: 40px;
+      border-radius: 50%;
+    }
+  }
+}
+</style>
--- src/views/main-page/index.vue
+++ src/views/main-page/index.vue
@@ -1,69 +1,29 @@
 <template>
   <div class="main-page-wrap">
     <el-container>
-      <el-header class="main-page-header">
-        <div class="header-logo">
-          <icon-svg :icon-class="iconClass"></icon-svg>
-          Lego H5
-        </div>
-        <div class="header-person">
-          <el-popover placement="bottom" width="100px" trigger="hover">
-            <ul class="header-person-menu">
-              <li>个人中心</li>
-              <li>退出登录</li>
-            </ul>
-            <div slot="reference">
-              <img src="../../assets/images/joker.jpeg" alt="" />
-            </div>
-          </el-popover>
-        </div>
-      </el-header>
+      <lego-header></lego-header>
       <home-page></home-page>
     </el-container>
   </div>
 </template>
 <script lang="ts">
 import homePage from "../home-page/index.vue";
+import legoHeader from "../../components/lego-header/index.vue";
 import { Component, Vue } from "vue-property-decorator";
 @Component({
   components: {
-    homePage
+    homePage,
+    legoHeader
   }
 })
-export default class MainPage extends Vue {
-  iconClass: string = "lego";
-}
+export default class MainPage extends Vue {}
 </script>
 <style lang="less" scoped>
 .main-page-wrap {
   width: 100%;
   height: 100%;
-  .main-page-header {
-    height: 50px !important;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    border-bottom: 1px solid #ccc;
-    .header-logo {
-      height: 100%;
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      cursor: pointer;
-    }
-    .header-person {
-      width: 44px;
-      height: 100%;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      cursor: pointer;
-      img {
-        width: 40px;
-        height: 40px;
-        border-radius: 50%;
-      }
-    }
+  /deep/ .el-container {
+    flex-direction: column;
   }
 }
 </style>
