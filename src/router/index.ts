4a175de5dd15d079b33b7368ed73ce2c65fae0e3
MagicHacker <zxmagichacker@gmail.com>
2020-01-20 14:50:16 +0800

    feat: 配置路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,25 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-
+import Login from "../views/login/index.vue";
+import mainPage from "../views/main-page/index.vue";
 Vue.use(VueRouter);
 
-const routes: any = [];
+const routes: any = [
+  {
+    path: "/login",
+    name: "login",
+    component: Login
+  },
+  {
+    path: "/mainPage",
+    name: "mainPage",
+    component: mainPage
+  },
+  {
+    path: "",
+    component: Login
+  }
+];
 
 const router = new VueRouter({
   routes
