ea673da31933053518bf86e1308dc26e4df99771
MagicHacker <zxmagichacker@gmail.com>
2020-01-20 14:14:18 +0800

    feat: 修改登录页样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="lego-login-wrap">
     <div class="lego-login-area">
-      <el-form :model="formData" label-width="70px" :rules="rules">
+      <el-form
+        :model="formData"
+        label-width="70px"
+        :rules="rules"
+        label-position="left"
+      >
         <el-form-item label="用户名" prop="username">
           <el-input v-model="formData.username"></el-input>
         </el-form-item>
         <el-form-item label="密码" prop="password">
           <el-input v-model="formData.password" show-password></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="signIn">登录</el-button>
-          <el-button type="info" @click="signUp">注册</el-button>
-        </el-form-item>
       </el-form>
+      <el-button type="primary" @click="signIn">登录</el-button>
+      <el-button type="info" @click="signUp">注册</el-button>
     </div>
   </div>
 </template>
@@ -49,17 +52,19 @@ export default class Login extends Vue {
   width: 100%;
   height: 100%;
   position: relative;
+  background: url("../../assets/images/login-bg.png") no-repeat center;
   .lego-login-area {
     width: 400px;
-    height: 300px;
+    height: 170px;
     position: absolute;
     left: 50%;
     top: 50%;
-    margin-top: -200px;
+    margin-top: -85px;
+    margin-left: -200px;
     border: 1px solid #ccc;
     border-radius: 10px;
-    box-sizing: border-box;
     padding: 20px 10px;
+    background-color: #fff;
   }
 }
 </style>
