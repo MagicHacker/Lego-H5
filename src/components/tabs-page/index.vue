dd980d7d01f79e56e3bd5c93301d49d56042dcca
Johnson <zxmagichacker@gmail.com>
2020-01-24 23:19:49 +0800

    feat: 构建tabs公共组件

---
+++ src/components/tabs-page/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-tabs v-model="activeTab" @tab-click="changeTab">
+    <el-tab-pane label="H5" name="1">H5</el-tab-pane>
+    <el-tab-pane label="长页H5" name="2">长页H5</el-tab-pane>
+  </el-tabs>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+// 定义ts接口interface
+interface Tabs {
+  name: string;
+}
+@Component
+export default class TabsPage extends Vue {
+  activeTab: string = "1";
+  changeTab(tab: Tabs): void {
+    this.activeTab = tab.name;
+  }
+}
+</script>
+<style lang="less" scoped></style>
