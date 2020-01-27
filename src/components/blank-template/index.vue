d51a0bef7dd2fd118fe290b9a228799587c2b2dd
Johnson <zxmagichacker@gmail.com>
2020-01-27 21:42:54 +0800

    feat: 构建空白模板组件

--- src/components/blank-template/index.vue
+++ src/components/blank-template/index.vue
@@ -1,8 +1,44 @@
 <template>
-  <div>空白模板页</div>
+  <div class="blank-tpl-wrap">
+    <div class="blank-tpl-item">
+      <icon-svg icon-class="create"></icon-svg>
+      <p>创建H5</p>
+    </div>
+  </div>
 </template>
 <script lang="ts">
-import Vue from "vue";
-export default Vue.extend({});
+import { Component, Vue } from "vue-property-decorator";
+export default class blankTemplate extends Vue {}
 </script>
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.blank-tpl-wrap {
+  width: 200px;
+  height: 250px;
+  background-color: #fff;
+  cursor: pointer;
+  border-radius: 4px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &:hover {
+    box-shadow: 0 0 16px 0 rgba(0, 0, 0, 0.16);
+    transform: translate3d(0, -2px, 0);
+  }
+  .blank-tpl-item {
+    width: 160px;
+    height: 210px;
+    border: 1px solid #ccc;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    p {
+      margin: 6px 0px 0px;
+    }
+    &:hover {
+      border-color: #25a589;
+      color: #25a589;
+    }
+  }
+}
+</style>
