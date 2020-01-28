e7d7d0ba21bce33a64a6cb9dd10f322f327ba7c1
Johnson <zxmagichacker@gmail.com>
2020-01-28 23:22:26 +0800

    feat: 添加内容模板

--- src/views/works-page/index.vue
+++ src/views/works-page/index.vue
@@ -3,20 +3,30 @@
     <tabs-page>
       <template v-slot:H5>
         <blank-template @createPage="createPage"></blank-template>
-        <content-template v-for="item in 10" :key="item"></content-template>
+        <content-template
+          v-for="item in listH"
+          :key="item.id"
+        ></content-template>
       </template>
       <template v-slot:longH5>
         <blank-template name="长页" @createPage="createPage"></blank-template>
-        <content-template v-for="item in 10" :key="item"></content-template>
+        <content-template
+          v-for="item in listLongH"
+          :key="item.id"
+        ></content-template>
       </template>
     </tabs-page>
   </div>
 </template>
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import tabsPage from "../../components/tabs-page/index.vue";
-import blankTemplate from "../../components/blank-template/index.vue";
-import contentTemplate from "../../components/content-template/index.vue";
+import { Component, Vue } from 'vue-property-decorator'
+import tabsPage from '../../components/tabs-page/index.vue'
+import blankTemplate from '../../components/blank-template/index.vue'
+import contentTemplate from '../../components/content-template/index.vue'
+// 活动页接口
+interface actPage {
+  id: string
+}
 @Component({
   components: {
     tabsPage,
@@ -25,20 +35,27 @@ import contentTemplate from "../../components/content-template/index.vue";
   }
 })
 export default class WorksPage extends Vue {
+  // H5数组
+  listH: Array<actPage> = []
+  listLongH: Array<actPage> = []
   createPage(name: string): void {
-    if (name === "长页") {
-      this.createLongH();
+    if (name === '长页') {
+      this.createLongH()
     } else {
-      this.createH();
+      this.createH()
     }
   }
   // 创建H5
   createH(): void {
-    alert("创建H5");
+    this.listH.push({
+      id: '1'
+    })
   }
   // 创建长页H5
   createLongH(): void {
-    alert("创建长页H5");
+    this.listLongH.push({
+      id: '2'
+    })
   }
 }
 </script>
