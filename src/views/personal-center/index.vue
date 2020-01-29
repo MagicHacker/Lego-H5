e5f7b33fe901a843966fe449b8825358bcc3db2d
Johnson <zxmagichacker@gmail.com>
2020-01-29 14:12:37 +0800

    feat: 构建个人中心组件

--- src/views/personal-center/index.vue
+++ src/views/personal-center/index.vue
@@ -1,12 +1,72 @@
 <template>
-  <div class="personal-center-wrap">个人中心</div>
+  <div class="personal-center-wrap">
+    <div class="personal-center-avatar">
+      <img :src="imgUrl" alt="" />
+    </div>
+    <el-form
+      :model="formData"
+      ref="ruleForm"
+      label-width="100px"
+      class="demo-ruleForm"
+    >
+      <el-form-item label="账号" prop="username">
+        <el-input v-model="formData.username"></el-input>
+      </el-form-item>
+      <el-form-item label="旧密码" prop="oldPassword">
+        <el-input v-model="formData.oldPassword"></el-input>
+      </el-form-item>
+      <el-form-item label="新密码" prop="newPassword">
+        <el-input v-model="formData.newPassword"></el-input>
+      </el-form-item>
+      <el-form-item label="上传头像">
+        <el-upload
+          action="https://jsonplaceholder.typicode.com/posts/"
+          :on-success="uploadAvatarSuccess"
+          :show-file-list="false"
+        >
+          <el-button>点击上传</el-button>
+        </el-upload>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">提交</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 <script lang="ts">
-import Vue from "vue";
-export default Vue.extend({});
+import { Component, Vue } from 'vue-property-decorator'
+@Component
+export default class PersonalCenter extends Vue {
+  // 头像地址
+  imgUrl: string = ''
+  formData = {
+    username: '',
+    oldPassword: '',
+    newPassword: ''
+  }
+  // 头像上传成功
+  uploadAvatarSuccess(res: any, file: any): void {
+    this.imgUrl = URL.createObjectURL(file.raw)
+  }
+}
 </script>
 <style lang="less" scoped>
 .personal-center-wrap {
-  background-color: #f0f3f4;
+  width: auto;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  .personal-center-avatar {
+    width: 80px;
+    height: 80px;
+    margin-bottom: 20px;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+    }
+  }
 }
 </style>
