2f1369e40f60e779a622778b90358aa6c892b692
Johnson <zxmagichacker@gmail.com>
2020-01-29 20:36:47 +0800

    feat: 左侧菜单切换组件

--- src/views/editor-aside/index.vue
+++ src/views/editor-aside/index.vue
@@ -1,15 +1,30 @@
 <template>
   <div class="editor-aside-wrap">
     <el-tabs tab-position="left">
-      <el-tab-pane label="组件列表">组件列表</el-tab-pane>
-      <el-tab-pane label="页面管理">页面管理</el-tab-pane>
-      <el-tab-pane label="模板列表">模板列表</el-tab-pane>
+      <el-tab-pane label="组件列表">
+        <component-lists></component-lists>
+      </el-tab-pane>
+      <el-tab-pane label="页面管理">
+        <page-manage></page-manage>
+      </el-tab-pane>
+      <el-tab-pane label="模板列表">
+        <template-lib></template-lib>
+      </el-tab-pane>
     </el-tabs>
   </div>
 </template>
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-@Component
+import componentLists from '../component-lists/index.vue'
+import pageManage from '../page-manage/index.vue'
+import templateLib from '../template-lib/index.vue'
+@Component({
+  components: {
+    componentLists,
+    pageManage,
+    templateLib
+  }
+})
 export default class EditorAside extends Vue {}
 </script>
 <style lang="less" scoped>
