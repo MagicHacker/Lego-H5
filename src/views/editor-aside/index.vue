7d4225b06c10e92b9240226d2c14093a64dc4d00
Johnson <zxmagichacker@gmail.com>
2020-01-29 20:07:37 +0800

    feat: 构建编辑页菜单，二级选项，主内容区，右侧面板

---
+++ src/views/editor-aside/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="editor-aside-wrap">
+    <el-menu default-active="0">
+      <el-menu-item index="0">组件列表</el-menu-item>
+      <el-menu-item index="1">页面管理</el-menu-item>
+      <el-menu-item index="2">模板库</el-menu-item>
+    </el-menu>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+@Component
+export default class EditorAside extends Vue {}
+</script>
+<style lang="less" scoped>
+.editor-aside-wrap {
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  /deep/ .el-menu {
+    height: 100%;
+  }
+}
+</style>
---
+++ src/views/editor-second-aside/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="editor-second-wrap">xxx</div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
+<style lang="less" scoped>
+.editor-second-wrap {
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+}
+</style>
