757c555aa79849323e6d54b34570dd36f0bccd12
Johnson <zxmagichacker@gmail.com>
2020-01-30 10:27:46 +0800

    feat: 构建属性面板组件

--- src/views/editor-right-panel/index.vue
+++ src/views/editor-right-panel/index.vue
@@ -1,13 +1,23 @@
 <template>
-  <div class="editor-right-wrap">aaa</div>
+  <div class="editor-right-wrap">
+    <el-tabs>
+      <el-tab-pane label="属性">请选中需要编辑的元件</el-tab-pane>
+      <el-tab-pane label="动画">请选中需要编辑的元件</el-tab-pane>
+      <el-tab-pane label="事件">请选中需要编辑的元件</el-tab-pane>
+      <el-tab-pane label="页面设置"></el-tab-pane>
+      <el-tab-pane label="JS脚本"></el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 <script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({})
+import { Component, Vue } from 'vue-property-decorator'
+@Component
+export default class EditorRightPanel extends Vue {}
 </script>
 <style lang="less" scoped>
 .editor-right-wrap {
   height: 100%;
   background-color: #fff;
+  padding: 0px 5px;
 }
 </style>
